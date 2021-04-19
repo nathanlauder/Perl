bff7ffc12b6390e7364b3cc217e22d5464b62355
Nathan Lauder <56481793+nlauder42@users.noreply.github.com>
2021-04-19 13:10:56 -0500

    destroying the object

--- objects.pl
+++ objects.pl
@@ -7,10 +7,14 @@ sub new {
     my $self = {
         _name => shift,
         _ssn => shift,
-        _height => shift;
-        _weight => shift;
     }
     bless $self, $class;
     # "fields" accessed with $self->{_name}
     return $self;
 }
+
+sub DESTROY {
+    print "destroys object";
+}
+
+my $person = new Person("Nathan", "123456789", )
