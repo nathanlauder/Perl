371f3c04dd8958e2acd2f851efdacc2dfd183c2a
Nathan Lauder <56481793+nathanlauder@users.noreply.github.com>
2021-04-25 12:24:12 -0500

    Update dataTypes.pl

--- dataTypes.pl
+++ dataTypes.pl
@@ -4,13 +4,21 @@ use warnings;
 # variables in general
 # "my" creates variable with a local scope
 # perl has strings, integer, floats
+
 my $netID = "YOUR_EMAIL";
 my $emailSuffix = '@creighton.edu';
 print("$netID$emailSuffix\n");
 
+
+my $z= 10;
+my $n = "foobar";
+my @array = ("something", 282005, 14.3);
+my %hash = ("Nathan" => "CompSci", "Taylor" => "Biology", "Lauren" => "Pharmacy");
+
+
 # basic arrays are symbolized with an @
 # and accessed by index with []
-my @array = ("something", 282005, 14.3);
+
 print "\$array[0] = $array[0]\n";
 print "\$array[1] = $array[1]\n";
 print "\$array[2] = $array[2]\n";
@@ -18,7 +26,6 @@ print "\$array[2] = $array[2]\n";
 # hashmaps/tables are symbolized with a %
 # and accessed by key with {}
 
-my %hash = ("Nathan" => "CompSci", "Taylor" => "Biology", "Lauren" => "Pharmacy");
 print("\$hash{Nathan} => $hash{Nathan}\n");
 print("\$hash{Taylor} => $hash{Taylor}\n");
 print("\$hash{Lauren} => $hash{Lauren}\n");
